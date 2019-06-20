5908ce782d935e3f10d78ac4155d49576f829738
Howy <Howy@Howys-MBP.local>
2019-06-20 19:07:19 -0400

    checker's logic is better now

--- js/checker.js
+++ js/checker.js
@@ -1,16 +1,14 @@
 // We receuve cards in ascending order of card strength
 
-var Checker = Checker || {};
-
 var checkValidity = function(prev, curr){
   dummyPair = [{ val: 1 }, { val: 1 }];
-  dummyCombo = [-7, 1, 5, 9, 13];
+  dummyCombo = [{ val: 1 }, { val: 1 }, { val: 5 }, { val: 5 }, { val: 9 }];
 
   if (prev.length == 0){
     if (curr.length == 1)
       return true;
     else if (curr.length == 2)
-      return checkPair(dummyPair, curr);
+      return isSameRank(curr[0], curr[1]);
     else if (curr.length == 5)
       return checkCombo(dummyCombo, curr);
     else
@@ -18,11 +16,11 @@ var checkValidity = function(prev, curr){
   }
   else if (curr.length != prev.length)
     return false;
-
   if (curr.length == 1)
     return checkHighCard(prev, curr);
-  else if (curr.length == 2)
+  else if (curr.length == 2){
     return checkPair(prev, curr);
+  }
   else if (curr.length == 5)
     return checkCombo(prev, curr);
   else
@@ -30,41 +28,36 @@ var checkValidity = function(prev, curr){
 }
 
 var checkHighCard = function(prev, curr){
-  return curr[0] > prev[0].val;
+  return curr[0].val > prev[0].val;
 }
 
 var checkPair = function(prev, curr){
-  if (Math.floor((curr[0]-1)/4) != Math.floor((curr[1]-1)/4))
+  console.log('issamerank is: ' + isSameRank(curr[0], curr[1]));
+  if (!isSameRank(curr[0], curr[1]))
     return false;
 
-  return curr[1] > prev[1].val;
+  return curr[1].val > prev[1].val;
 }
 
 var checkCombo = function(prev, curr){
   var prevStrength = 0;
   var currStrength = 0;
-  //Check for straight
+
   if (checkStraight(prev))
     prevStrength += 3;
-  else if (checkStraight(curr))
+  if (checkStraight(curr))
     currStrength += 3;
-
-  //Check for flush
   if (checkFlush(prev))
     prevStrength += 4;
-  else if (checkFlush(curr))
+  if (checkFlush(curr))
       prevStrength += 4;
-
-  // Check for full house
   if (checkFullHouse(prev))
     prevStrength += 5;
-  else if (checkFullHouse(curr))
+  if (checkFullHouse(curr))
     currStrength += 5;
-
-  // CHeck for 4 of a kind
   if (checkFourOfAKind(prev))
     prevStrength += 6;
-  else if (checkFourOfAKind(curr))
+  if (checkFourOfAKind(curr))
     currStrength += 6;
 
   if (prevStrength < currStrength)
@@ -72,60 +65,67 @@ var checkCombo = function(prev, curr){
   else if (prevStrength > currStrength)
     return false;
   else if (prevStrength == 3 || prevStrength == 4 || prevStrength == 7)
-    return curr[4] > prev[4];
+    return curr[4].val > prev[4].val;
   else{
     if (checkThreeOfAKind(curr) && checkThreeOfAKind(prev))
-      return curr[0] > prev[0];
+      return curr[0].val > prev[0].val;
     else if (checkThreeOfAKind(curr) && checkThreeOfAKind(prev.splice(2)))
-      return curr[0] > prev[5];
+      return curr[0].val > prev[4].val;
     else if (checkThreeOfAKind(curr.splice(2)) && checkThreeOfAKind(prev.splice(2)))
-      return curr[5] > prev[5];
+      return curr[4].val > prev[4].val;
     else
-      return curr[5] > prev[0];
+      return curr[4].val > prev[0].val;
   }
 }
 
 var checkStraight = function(arr){
-  return (isEqual(arr[0]+1, arr[1]) &&
-          isEqual(arr[1]+1, arr[2]) &&
-          isEqual(arr[2]+1, arr[3]) &&
-          isEqual(arr[3]+1, arr[4]));
+  return (rankDiffOne(arr[0], arr[1]) &&
+          rankDiffOne(arr[1], arr[2]) &&
+          rankDiffOne(arr[2], arr[3]) &&
+          rankDiffOne(arr[3], arr[4]));
 }
 
 var checkFlush = function(arr){
-  return (isEqual(arr[0]%4, arr[1]%4) &&
-          isEqual(arr[1]%4, arr[2]%4) &&
-          isEqual(arr[2]%4, arr[3]%4) &&
-          isEqual(arr[3]%4, arr[4]%4));
+  return (isSameSuit(arr[0], arr[1]) &&
+          isSameSuit(arr[1], arr[2]) &&
+          isSameSuit(arr[2], arr[3]) &&
+          isSameSuit(arr[3], arr[4]));
 }
 
 var checkFullHouse = function(arr){
-  return ((isEqual(arr[0], arr[1]) &&
-           isEqual(arr[1], arr[2]) &&
-           isEqual(arr[3], arr[4]))
+  return ((isSameRank(arr[0], arr[1]) &&
+           isSameRank(arr[1], arr[2]) &&
+           isSameRank(arr[3], arr[4]))
           ||
-          (isEqual(arr[0], arr[1]) &&
-           isEqual(arr[2], arr[3]) &&
-           isEqual(arr[3], arr[4])));
-}
-
-var checkFourOfAKind = function(arr){
-  return ((isEqual(arr[1], arr[2]) &&
-           isEqual(arr[2], arr[3]) &&
-           isEqual(arr[3], arr[4]))
-          ||
-          (isEqual(arr[0], arr[1]) &&
-           isEqual(arr[1], arr[2]) &&
-           isEqual(arr[2], arr[3])));
+          (isSameRank(arr[0], arr[1]) &&
+           isSameRank(arr[2], arr[3]) &&
+           isSameRank(arr[3], arr[4])));
 }
 
 var checkThreeOfAKind = function(arr){
-  return (isEqual(arr[0], arr[1]) &&
-          isEqual(arr[1], arr[2]) &&
-          isEqual(arr[2], arr[3]));
+  return (isSameRank(arr[0], arr[1]) &&
+          isSameRank(arr[1], arr[2]) &&
+          isSameRank(arr[2], arr[3]));
 }
 
+var checkFourOfAKind = function(arr){
+  return ((isSameRank(arr[1], arr[2]) &&
+           isSameRank(arr[2], arr[3]) &&
+           isSameRank(arr[3], arr[4]))
+          ||
+          (isSameRank(arr[0], arr[1]) &&
+           isSameRank(arr[1], arr[2]) &&
+           isSameRank(arr[2], arr[3])));
+}
 
-var isEqual = function(first, second){
-  return (first == second);
+var isSameSuit = function(first, second){
+  return (first.val%4 == second.val%4);
+}
+
+var isSameRank = function(first, second){
+  return (Math.floor((first.val-1)/4) == Math.floor((second.val-1)/4));
+}
+
+var rankDiffOne = function(first, second){
+  return (Math.floor((first.val-1)/4) + 1 == Math.floor((second.val-1)/4));
 }
